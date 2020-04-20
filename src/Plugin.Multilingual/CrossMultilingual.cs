dc92d708d8f3db43c932f7c918bd03872981bd06
David Thompson <>
2020-04-20 06:57:15 +0100

    Moved shared code into core project

---
+++ src/Plugin.Multilingual/CrossMultilingual.cs
@@ -0,0 +1,46 @@
+﻿using Plugin.Multilingual.Abstractions;
+using System;
+using System.Globalization;
+
+namespace Plugin.Multilingual
+{
+  /// <summary>
+  /// Cross platform Multilingual implemenations
+  /// </summary>
+  public class CrossMultilingual
+  {
+    static Lazy<IMultilingual> Implementation = new Lazy<IMultilingual>(() => CreateMultilingual(), System.Threading.LazyThreadSafetyMode.PublicationOnly);
+
+    /// <summary>
+    /// Current settings to use
+    /// </summary>
+    public static IMultilingual Current
+    {
+      get
+      {
+        var ret = Implementation.Value;
+        if (ret == null)
+        {
+          throw NotImplementedInReferenceAssembly();
+        }
+        return ret;
+      }
+    }
+
+    static IMultilingual CreateMultilingual()
+    {
+        #if NETSTANDARD1_0
+                    return null;
+        #else
+        #pragma warning disable IDE0022 // Use expression body for methods
+			        return new MultilingualImplementation();
+        #pragma warning restore IDE0022 // Use expression body for methods
+        #endif
+     }
+
+    internal static Exception NotImplementedInReferenceAssembly()
+    {
+      return new NotImplementedException("This functionality is not implemented in the portable version of this assembly.  You should reference the NuGet package from your main application project in order to reference the platform-specific implementation.");
+    }
+  }
+}
